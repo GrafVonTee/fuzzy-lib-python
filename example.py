a08329c00c1c807fc6942c043e6015e0f6f96ac2
Graf von Tee <https://github.com/GrafVonTee>
2024-03-14 02:17:48 +0300

    Added Gaussian Term

--- example.py
+++ example.py
@@ -1,3 +1,21 @@
-from fuzzy_lib import Term
+from fuzzy_lib.term import Gaussian
 import numpy as np
 import matplotlib.pyplot as plt
+
+x_array = np.arange(0, 300+1)
+
+thurst_low = Gaussian("low", 0, 300, 0, 65)
+thurst_low.make_range()
+
+thurst_mid = Gaussian("mid", 0, 300, 150, 65)
+thurst_mid.make_range()
+
+thurst_high = Gaussian("high", 0, 300, 300, 65)
+thurst_high.make_range()
+
+plt.plot(x_array, thurst_low.value_range, label=thurst_low.name)
+plt.plot(x_array, thurst_mid.value_range, label=thurst_mid.name)
+plt.plot(x_array, thurst_high.value_range, label=thurst_high.name)
+
+plt.legend()
+plt.show()
--- fuzzy_lib/term.py
+++ fuzzy_lib/term.py
@@ -3,19 +3,60 @@
 Some Terms classes and functions here
 """
 import numpy as np
+import math
 
 
 class Term:
     """Just a basic term class"""
     def __init__(self, name: str, lower_bound: int, upper_bound: int):
-        self.name = name
-        self.lower_bound = lower_bound
-        self.upper_bound = upper_bound
-        self.value_range = np.zeros(upper_bound - lower_bound + 1)
+        self.name: str = name
+        self.lower_bound: int = lower_bound
+        self.upper_bound: int = upper_bound
+        self.value_range = np.zeros(0)
 
-    def MakeRange(self):
-        pass
+    """Initialize value_range with defaults"""
+    def make_range(self):
+        self.value_range = np.zeros(self.upper_bound - self.lower_bound + 1)
+
+    """
+    """
+    def get_value_range(self):
+        return self.value_range
+
+    """
+    """
+    def get_degree_of_membership(self, x: int) -> float:
+        if x < self.lower_bound or x > self.upper_bound:
+            return 0.0
+
+        return self.value_range[x - self.lower_bound]
 
 
 class Gaussian(Term):
-    pass
+    """Gaussian Term class"""
+    def __init__(self,
+                 name: str,
+                 lower_bound: int,
+                 upper_bound: int,
+                 mean: int,
+                 variance: float):
+        super().__init__(name, lower_bound, upper_bound)
+        self.mean = mean
+        self.variance = variance
+
+    def make_range(self):
+        super().make_range()
+
+        max_value: float = 0
+        temp_mean = self.mean - self.lower_bound
+
+        for i, x in np.ndenumerate(self.value_range):
+            self.value_range[i] = math.exp(
+                -1 * math.pow(i[0] - temp_mean, 2)
+                / (2 * math.pow(self.variance, 2))
+            )
+
+            if max_value < self.value_range[i]:
+                max_value = self.value_range[i]
+
+        self.value_range /= max_value
